ed33e12f97246a531f1156ed0bc0b770d7243615
gia0901 <gianguyen.hcmut@gmail.com>
2023-12-18 22:31:09 +0700

    update rcc driver

--- inc/stm32f407xx_rcc.h
+++ inc/stm32f407xx_rcc.h
@@ -10,9 +10,120 @@
 
 #include "stm32f407xx.h"
 
+typedef struct
+{
+	uint8_t PLLState;
+	uint8_t PLLSource;
+	uint8_t PLLM;
+	uint8_t PLLN;
+} RCC_PLLInit_t;
+
+typedef struct
+{
+	uint8_t OscType;
+	RCC_PLLInit_t PLL;
+} RCC_OscInit_t;
 
 
-uint32_t RCC_Get_PCLK1_Value();	// APB1
-uint32_t RCC_Get_PCLK2_Value();	// APB2
+
+typedef struct
+{
+	uint8_t ClockType;			// Clock to be configured
+	uint32_t SysclkSource;		// Clock will be supplied for System clock
+	uint8_t APB1ClockDivider;	//
+	uint8_t APB2ClockDivider;
+	uint8_t AHBClockDivider;
+} RCC_ClkInit_t;
+
+uint8_t RCC_GetFlagStatus(uint32_t FlagName);
+void RCC_OscInit(RCC_OscInit_t *pOscHandle);
+void RCC_ClkInit(RCC_ClkInit_t *pOscHandle);
+
+
+uint32_t RCC_Get_SYSCLK_Value();	// System clock
+uint32_t RCC_Get_HCLK_Value();		// AHB clock
+uint32_t RCC_Get_PCLK1_Value();		// APB1 clock
+uint32_t RCC_Get_PCLK2_Value();		// APB2 clock
+
+
+
+
+
+
+/****************** Oscillator Configuration Macros *********************/
+
+/* Osc Type */
+#define RCC_OSC_TYPE_HSI	0
+#define RCC_OSC_TYPE_HSE	1
+
+/* HSI state */
+#define RCC_HSI_OFF			DISABLE
+#define RCC_HSI_ON			ENABLE
+
+/* HSE state */
+#define RCC_HSE_OFF			DISABLE
+#define RCC_HSE_ON			ENABLE
+
+/* PLL state */
+#define RCC_PLL_OFF			DISABLE
+#define RCC_PLL_ON			ENABLE
+
+/* PLL source */
+#define RCC_PLL_SOURCE_HSI	0
+#define RCC_PLL_SOURCE_HSE	1
+
+
+
+/****************** Clock Configuration Macros *********************/
+
+/* SYSCLK source */
+#define RCC_SYSCLK_SOURCE_HSI		0
+#define RCC_SYSCLK_SOURCE_HSE		1
+#define RCC_SYSCLK_SOURCE_PLLCLK	2
+
+/* Clock Type */
+#define RCC_CLOCKTYPE_SYSCLK		1
+#define RCC_CLOCKTYPE_HCLK			2
+#define RCC_CLOCKTYPE_PCLK1			4
+#define RCC_CLOCKTYPE_PCLK2			8
+
+/* AHB Clock Divider --> APB1/APB2 clock */
+#define RCC_AHB_DIV1				0
+#define RCC_AHB_DIV2				4
+#define RCC_AHB_DIV4				5
+#define RCC_AHB_DIV8				6
+#define RCC_AHB_DIV16				7
+
+/* SYSCLK Divider --> AHB clock */
+#define RCC_SYSCLK_DIV1				0
+#define RCC_SYSCLK_DIV2				8
+#define RCC_SYSCLK_DIV4				9
+#define RCC_SYSCLK_DIV8				10
+#define RCC_SYSCLK_DIV16			11
+#define RCC_SYSCLK_DIV64			12
+#define RCC_SYSCLK_DIV128			13
+#define RCC_SYSCLK_DIV256			14
+#define RCC_SYSCLK_DIV512			15
+
+/* MCO1 Source & Divider */
+#define RCC_MCO1_SOURCE_HSI			0
+#define RCC_MCO1_SOURCE_HSE			2
+#define RCC_MCO1_SOURCE_PLL			3
+
+#define RCC_MCO1_DIV1				0
+#define RCC_MCO1_DIV2				4
+#define RCC_MCO1_DIV3				5
+#define RCC_MCO1_DIV4				6
+#define RCC_MCO1_DIV5				7
+
+/* Flash Latency */
+#define RCC_FLASH_ACR_0WS			0
+
+
+/**********************  Flag status  ***********************/
+
+#define RCC_FLAG_HSI_READY	(1 << RCC_CR_HSIRDY)
+#define RCC_FLAG_HSE_READY	(1 << RCC_CR_HSERDY)
+#define RCC_FLAG_PLL_READY	(1 << RCC_CR_PLLRDY)
 
 #endif /* INC_STM32F407XX_RCC_H_ */
--- src/stm32f407xx_rcc.c
+++ src/stm32f407xx_rcc.c
@@ -7,55 +7,260 @@
 #include "stm32f407xx_rcc.h"
 
 
-uint16_t AHB_prescaler[8] = {2, 4, 8, 16, 64, 128, 256, 512};
-uint8_t  APB_prescaler[4] = {2, 4, 8, 16};
+uint16_t AHB_PreTable[8] = {2, 4, 8, 16, 64, 128, 256, 512};
+uint8_t  APB_PreTable[4] = {2, 4, 8, 16};
 
-uint32_t RCC_Get_PCLK1_Value()
+
+static void RCC_HSI_Enable(void)
 {
-	uint8_t check_system_clk;
-	uint32_t SysClk_freq, Pclk1_freq;
-	uint16_t apb1_pre, ahb1_pre;
+	// Enable HSI
+	RCC->RCC_CR |= (1 << RCC_CR_HSION);
+	// Wait until HSI is ready
+	while (RCC_GetFlagStatus(RCC_FLAG_HSI_READY) == FLAG_NOT_SET);
+}
+
+static void RCC_HSE_Enable(void)
+{
+	// Enable HSE
+	RCC->RCC_CR |= (1 << RCC_CR_HSEON);
+	while (RCC_GetFlagStatus(RCC_FLAG_HSE_READY) == FLAG_NOT_SET);
+}
+
+static void RCC_PLL_Enable(void)
+{
+	// Enable PLL
+	RCC->RCC_CR |= (1 << RCC_CR_PLLON);
+	while (RCC_GetFlagStatus(RCC_FLAG_PLL_READY) == FLAG_NOT_SET);
+}
+
+static void RCC_PLL_Config(RCC_PLLInit_t PLL)
+{
+
+}
+
+void RCC_OscInit(RCC_OscInit_t *pOscHandle)
+{
+	/* PLL is not used */
+	if (pOscHandle->PLL.PLLState != RCC_PLL_ON)
+	{
+		if (pOscHandle->OscType == RCC_OSC_TYPE_HSI)
+		{
+			RCC_HSI_Enable();
+		}
+		else if (pOscHandle->OscType == RCC_OSC_TYPE_HSE)
+		{
+			RCC_HSE_Enable();
+		}
+	}
+	/* PLL is used */
+	else if (pOscHandle->PLL.PLLState == RCC_PLL_ON)
+	{
+		/* Configure PLL parameters */
+		RCC_PLL_Config(pOscHandle->PLL);
+
+		/* Configure PLL source */
+		if (pOscHandle->PLL.PLLSource == RCC_PLL_SOURCE_HSI)
+		{
+			/* Enable HSI */
+			RCC_HSI_Enable();
+
+			/* Set HSI as PLL source. Must set before enabling PLL */
+			RCC->RCC_PLLCFGR &= ~(1 << RCC_PLLCFGR_PLLSRC);
+		}
+		else if (pOscHandle->PLL.PLLSource == RCC_PLL_SOURCE_HSE)
+		{
+			/* Enable HSE */
+			RCC_HSE_Enable();
+
+			/* Set HSE as PLL source. Must set before enabling PLL */
+			RCC->RCC_PLLCFGR |= (1 << RCC_PLLCFGR_PLLSRC);
+		}
+
+		/* Enable PLL */
+		RCC_PLL_Enable();
+
+		/* Wait for Flash Latency */
+
+
+	}
+
+}
+
+void RCC_ClkInit(RCC_ClkInit_t *pClkHandle)
+{
+	/******************* Init HCLK = SYCLK / divider *******************/
+	if ((pClkHandle->ClockType & RCC_CLOCKTYPE_HCLK) == RCC_CLOCKTYPE_HCLK)
+	{
+		RCC->RCC_CFGR |= (pClkHandle->AHBClockDivider << RCC_CFGR_HRPE);
+	}
+
+
+	/******************* PCLK1 (APB1 clock = AHB / divider) Configuration *******************/
+	if ((pClkHandle->ClockType & RCC_CLOCKTYPE_PCLK1) == RCC_CLOCKTYPE_PCLK1)
+	{
+		/* Set the divider */
+		RCC->RCC_CFGR |= (pClkHandle->APB1ClockDivider << RCC_CFGR_PPRE1);
+	}
+
+	/******************* PCLK2 (APB2 clock) Configuration *******************/
+	if ((pClkHandle->ClockType & RCC_CLOCKTYPE_PCLK1) == RCC_CLOCKTYPE_PCLK2)
+	{
+		/* Set the divider */
+		RCC->RCC_CFGR |= (pClkHandle->APB2ClockDivider << RCC_CFGR_PPRE2);
+	}
+
+	/******************* SYSCLK (Clock source) Configuration *******************/
+
+	/* Wait until the Clock is ready */
+	if (pClkHandle->SysclkSource == RCC_SYSCLK_SOURCE_HSI)
+	{
+		/* Wait until HSI is ready */
+		while (RCC_GetFlagStatus(RCC_FLAG_HSI_READY) == FLAG_NOT_SET);
+	}
+	else if (pClkHandle->SysclkSource == RCC_SYSCLK_SOURCE_HSE)
+	{
+		/* Wait until HSE is ready */
+		while (RCC_GetFlagStatus(RCC_FLAG_HSE_READY) == FLAG_NOT_SET);
+	}
+	else if (pClkHandle->SysclkSource == RCC_SYSCLK_SOURCE_PLLCLK)
+	{
+		/* Wait until PLL is ready */
+		while (RCC_GetFlagStatus(RCC_FLAG_PLL_READY) == FLAG_NOT_SET);
+	}
+
+	/* Set the clock as SYSCLK System Clock */
+	RCC->RCC_CFGR |= (pClkHandle->SysclkSource << RCC_CFGR_SW);
+
+
+
+}
+
+/*
+ * MCO functions must be configured before enabling the clock or PLL
+ */
+void RCC_MCO1Config(uint8_t MCO1Source, uint8_t MCO1Div)
+{
+	uint32_t tempreg = 0;
+
+	tempreg |= (MCO1Source << RCC_CFGR_MCO1);
+	tempreg |= (MCO1Div << RCC_CFGR_MCO1PRE);
+
+	RCC->RCC_CFGR &= ~(0x3 << RCC_CFGR_MCO1);		// Clear 2 bits of MCO1 field
+	RCC->RCC_CFGR &= ~(0x7 << RCC_CFGR_MCO1PRE);	// Clear 2 bits of MCO1 prescaler field
+
+	RCC->RCC_CFGR |= tempreg;
+
+}
+
+uint32_t RCC_Get_PLL_Value()
+{
+
+	return 0;
+}
+
+uint32_t RCC_Get_SYSCLK_Value()
+{
+	uint8_t check_sysclk_source;
+	uint32_t sysclk_freq;
+
+	check_sysclk_source = (RCC->RCC_CFGR >> RCC_CFGR_SWS) & 0x3;
+	if (check_sysclk_source == 0)
+	{
+		// HSI is used as system clock source
+		sysclk_freq = 16000000;
+	}
+	else if (check_sysclk_source == 1)
+	{
+		// HSE is used as system clock source
+		sysclk_freq = 8000000;
+	}
+	else if (check_sysclk_source == 2)
+	{
+		// PLL is used as system clock source
+		sysclk_freq = RCC_Get_PLL_Value();
+	}
+	return sysclk_freq;
+}
+
+/*
+ * Note: HCLK (AHB clock) = Sysclock / AHB prescaler
+ */
+uint32_t RCC_Get_HCLK_Value()
+{
+	uint32_t sysclk_freq, hclk_freq;
+	uint8_t ahb1_pre;
 	uint8_t temp;
 
-	/* Check which clock source is the system clock */
-	check_system_clk = (RCC->RCC_CFGR >> RCC_CFGR_SWS) & 0x3;
-	if (check_system_clk == 0)
-	{
-		// HSI is used: 16 Mhz
-		SysClk_freq = 16000000;
-	}
-	else if (check_system_clk == 1)
-	{
-		// HSE is used: 8 Mhz
-		SysClk_freq = 8000000;
-	}
+	sysclk_freq = RCC_Get_SYSCLK_Value();
 
-	/* Calculate AHB prescaler */
+	/* Get AHB prescaler value */
 	temp = (RCC->RCC_CFGR >> RCC_CFGR_HRPE) & 0xF;
 
 	if (temp < 8)
-	{
 		ahb1_pre = 1;
-	}
 	else
-	{
-		ahb1_pre = AHB_prescaler[temp - 8];	// temp = 9, 10, 11,...
-	}
+		ahb1_pre = AHB_PreTable[temp - 8];
+
+
+	/* Calculate HCLK frequency */
+	hclk_freq = sysclk_freq / ahb1_pre;
+
+	return hclk_freq;
+}
+
+
+/*
+ * Note: PCLK1 (APB1 clock) = HCLK / APB1 Prescaler
+ */
+uint32_t RCC_Get_PCLK1_Value()
+{
+	uint32_t hclk_freq, pclk1_freq;
+	uint16_t apb1_pre;
+	uint8_t temp;
+
+	hclk_freq = RCC_Get_HCLK_Value();
 
 	/* Calculate APB1 prescaler */
 	temp = (RCC->RCC_CFGR >> RCC_CFGR_PPRE1) & 0x7;
 
 	if (temp < 4)
-	{
 		apb1_pre = 1;
-	}
 	else
-	{
-		apb1_pre = APB_prescaler[temp - 4];
-	}
+		apb1_pre = APB_PreTable[temp - 4];
 
 	/* Calculate Pclk1 frequency */
-	Pclk1_freq = (SysClk_freq / ahb1_pre) / apb1_pre;
+	pclk1_freq = hclk_freq / apb1_pre;
 
-	return Pclk1_freq;
+	return pclk1_freq;
 }
+
+uint32_t RCC_Get_PCLK2_Value()
+{
+	uint32_t hclk_freq, pclk2_freq;
+	uint16_t apb2_pre;
+	uint8_t temp;
+
+	hclk_freq = RCC_Get_HCLK_Value();
+
+	temp = (RCC->RCC_CFGR >> RCC_CFGR_PPRE2) & 0x7;
+	if (temp < 4)
+		apb2_pre = 1;
+	else
+		apb2_pre = APB_PreTable[temp - 4];
+
+	pclk2_freq = hclk_freq / apb2_pre;
+
+	return pclk2_freq;
+}
+
+
+
+uint8_t RCC_GetFlagStatus(uint32_t FlagName)
+{
+	if (RCC->RCC_CR & FlagName)
+	{
+		return FLAG_SET;
+	}
+	return FLAG_NOT_SET;
+}
+
